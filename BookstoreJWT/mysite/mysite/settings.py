4b06dddd76f371b0a52bdf6eca5525eea0c294f2
Oriol <oriol.luis.boiria@gmail.com>
2022-08-08 17:28:14 +0200

    instalacion de las apps de autenticación de django

--- BookstoreJWT/mysite/mysite/settings.py
+++ BookstoreJWT/mysite/mysite/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api_bookstore.apps.ApiBookstoreConfig',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
 ]
 
 MIDDLEWARE = [
