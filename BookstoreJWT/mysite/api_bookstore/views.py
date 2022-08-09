000a383b6690f196a2e1aa4c0fc58840c0ec76bf
Oriol <oriol.luis.boiria@gmail.com>
2022-08-09 18:25:09 +0200

    restricciones al conjunto de vistas y migración

--- BookstoreJWT/mysite/api_bookstore/views.py
+++ BookstoreJWT/mysite/api_bookstore/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import AuthorSerializer, BookSerializer
 from .models import Author, Book
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -12,3 +16,14 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('title')
     serializer_class = BookSerializer
+
+
+@api_view(["GET"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def welcome(request):
+    content = {"message": "Welcome to the BookStore!"}
+    return JsonResponse(content)
+
+
+
